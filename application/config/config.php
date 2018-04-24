8b1cf2b77b41df6ed1ed60fe55e976f959ee9480
Victor Oshiro <victor.za.oshiro5@gmail.com>
2018-04-24 18:17:56 -0300

    ajustes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ec2-18-188-16-107.us-east-2.compute.amazonaws.com/keepbox/' ;
+$config['base_url'] = 'http://ec2-18-188-16-107.us-east-2.compute.amazonaws.com/' ;
 
 /*
 |--------------------------------------------------------------------------
