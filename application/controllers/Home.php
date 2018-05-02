7da7ba81b6c06e72ef74b2d2dfd9758e03d0ea17
mdd300 <vo12345>
2018-05-02 13:09:34 -0300

    Revert "Merge branch 'master' of https://github.com/mdd300/keepbox"
    This reverts commit c666022a1b6e1e20d26083ded55d2f90a03d6aba, reversing
    changes made to a4752f8e2f553b3989c0c5833afae4f5b37f942d.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -25,7 +25,11 @@ class Home extends CI_Controller {
 		$this->load->view('metronic/structure/footer_default');*/
 
         $this->load->view('Keep/structure/header_default');
+<<<<<<< HEAD
         $this->load->view('Keep/Como_Funciona/Como_Funciona');
+=======
+        $this->load->view('Keep/Servicos/Servicos');
+>>>>>>> parent of c666022... Merge branch 'master' of https://github.com/mdd300/keepbox
         $this->load->view('Keep/structure/footer_default');
 
     }
@@ -52,81 +56,4 @@ class Home extends CI_Controller {
 
     }
 
-    public function cadastro_email($Data = null){
-        if ($Data == null) {
-            $Output = true;
-            $Data = $this->input->post();
-        } else {
-            $Output = false;
-        }
-
-        $this->load->model('Home_model');
-
-        $retorno['success'] = $this->Home_model->Cadastro_model($Data['Data']);
-
-        if ($Output == true) {
-            echo json_encode($retorno);
-        } else {
-            return $retorno;
-        }
-
-    }
-
-    public function EnviarEmail()
-    {
-        // the message
-        $msg = "
-        
-        <html>
-<head>
-    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
-    <title>Enviando emails com a library nativa do CodeIgniter</title>
-</head>
-<body>
-<table cellspacing='0' cellpadding='0' border='0' width='100%'>
-    <tr>
-        <td class='navbar navbar-inverse' align='center'>
-            <!-- This setup makes the nav background stretch the whole width of the screen. -->
-            <table width='650px' cellspacing='0' cellpadding='3' class='container'>
-                <tr class='navbar navbar-inverse'>
-                    <td>teste</td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor='#FFFFFF' align='center'>
-            <table width='650px' cellspacing='0' cellpadding='3' class='container'>
-                <tr>
-                    <td><Teste</td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor='#FFFFFF' align='center'>
-            <table width='650px' cellspacing='0' cellpadding='3' class='container'>
-                <tr>
-                    <td>
-                        <hr>
-                        <p>&copy; Universidade CodeIgniter - 2016</p>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
-</body>
-</html>
-        
-        ";
-
-// use wordwrap() if lines are longer than 70 characters
-        $msg = wordwrap($msg,70);
-
-// send email
-        mail("victor.za.oshiro5@gmail.com","My subject",$msg);
-    }
-
 }
-
--- public/assets/metronic/custom/js/angular/controllers/Cadastro.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('app_landing').controller('cadastro_ctrl', ['$scope', '$http', function ($scope, $http) {
-
-    console.log("teste")
-
-}])
