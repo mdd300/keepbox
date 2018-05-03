62bb8bb3f538434e27d34cf23690e4ea23fc0d8c
mdd300 <vo12345>
2018-05-03 13:24:43 -0300

    Mudança Pag Como Funciona

--- application/views/Keep/Como_Funciona/Como_Funciona.php
+++ application/views/Keep/Como_Funciona/Como_Funciona.php
@@ -12,43 +12,49 @@
         
     </ul>
 
-<div style="padding-top: 20% " class="text-font-sans">
+<div class="text-font-sans content-li-center-vert" >
 
-     <ul class="content-ul-simulator-data">
+    <ul class="content-ul-data-serv">
         <li>
-           <div class="color-text-green">1.Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
-           <div class="color-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
+           <div class="color-text-green text-title-sm step-text">Como comprar com a KeepBox</div> 
+            
+        </li>
+    </ul>
+     <ul class="content-ul-data-serv">
+        <li>
+            
+           <div class="color-text-grey-light" style="padding-top: 10px">1 -  Crie sua conta KeepBox gratuitamente e compre nas lojas online dos EUA. </div> 
         </li>
     </ul>
 
-    <ul class="content-ul-simulator-data">
+    <ul class="content-ul-data-serv">
         <li>
-           <div class="color-text-green">2.Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
-           <div class="color-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
+            
+           <div class="color-text-grey-light" style="padding-top: 10px"
+>2 – Mande entregar suas compras no endereço da sua conta KeepBox 
+           </div> 
+                                        
+        
         </li>
     </ul>
 
-    <ul class="content-ul-simulator-data">
+    <ul class="content-ul-data-serv">
         <li>
-           <div class="color-text-green">3.Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
-           <div class="color-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
+            
+           <div class="color-text-grey-light"style="padding-top: 10px" >3 – Recebemos e juntamos suas encomendas em uma única caixa, sem custo adicional
+*(Assim, você paga um único frete e pode economizar até 80% no valor de envio)
+</div> 
         </li>
     </ul>
 
-    <ul class="content-ul-simulator-data">
+    <ul class="content-ul-data-serv">
         <li>
-           <div class="color-text-green">4.Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
-           <div class="color-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
+            
+           <div class="color-text-grey-light"style="padding-top: 10px" >4 – Enviamos para você, onde quiser, pelos melhores preços 
+</div> 
         </li>
     </ul>
 
-    <ul class="content-ul-simulator-data">
-        <li>
-           <div class="color-text-green">5.Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
-           <div class="color-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div> 
-        </li>
-    </ul>
-    
 
 </div>
 
@@ -64,74 +70,55 @@
         <button class="btn-config-2 color-background-roxo color-text-white text-1">Confira Aqui</button></div>
  </div>
 
-  <div style="padding-top: 10%" class="text-font-sans text-title-sm content-align-tutorial-module3">
-     <div class="color-text-grey-light">QUEM SOMOS?
-     </div>
-</div>
-
-<div class="content-li-info-data-cf">
-    <div class="text-1 text-font-sans color-text-grey-light padding-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum
-    </div>
-    
-</div>
-<br>
-
-<div class="align-x-center" style="padding-top: 10%" >
-     <img class="logo-size" src='<?= base_url() ?>public/assets/metronic/custom/img/LOGOTIPO.png'>
-</div>
-
-<div>
-   <div >
-            <li class="content-li-info-data-cf">
-                 <div class="box-placeholder color-background-white" style="background-color: green">
-            <!---->
-            <!--Imagem aqui-->
-            <!---->
-                </div> 
-               
-            </li>
-            <li class="content-li-info-data-sm-cf " style="margin-left: 60%">
-                <div class="text-title-1 color-text-green color-text-green text-bold text-font-sans">
-                    SAULO ARRUDA
-                </div>
-                <div class="text-1 text-font-sans color-text-grey" style="padding-top: 20px">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-                
-            </li>
-    </div> 
-
-     <div style="padding-top: 26%">
-            <li class="content-li-info-data-cf" >
-                 <div class="box-placeholder color-background-white" style="background-color: green ;margin-left: 729px; margin-bottom: 25px"
-                 >
-            <!---->
-            <!--Imagem aqui-->
-            <!---->
-                </div> 
-               
-            </li>
-            <li class="content-li-info-data-sm-cf " style="margin-left: 381px" >
-                <div class="text-title-1 color-text-green color-text-green text-bold text-font-sans">
-                    YANN RODRIGUES
-                </div>
-                <div class="text-1 text-font-sans color-text-grey" style="padding-top: 20px">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-                
-            </li>
-    </div> 
-
+ <div class="content-simulator-module4-serv ">
+  
+        <div class="color-text-roxo text-title-md text-font-sans">COMPRA ASSISTIDA / PERSONAL SHOPPER</div>
+        <div class="color-text-roxo text-2 text-font-sans" > Me ajude nas compras</div>
 
 </div>
 
+<div class="text-font-sans content-li-center-vert-less-pad" >
+
+   
+     <ul class="content-ul-data-serv">
+        <li>
+            
+           <div class="color-text-grey-light" style="padding-top: 10px">1 – Você nos envia quais itens deseja comprar, quantidade e tamanho.
+(a maneira mais fácil é copier e colar os links exatos de cada produto)  
+</div> 
+        </li>
+    </ul>
+
+    <ul class="content-ul-data-serv">
+        <li>
+            
+           <div class="color-text-grey-light" style="padding-top: 10px"
+>2 – Nós faremos um orçamento completo para você e lhe enviaremos para aprovação
+           </div> 
+                                        
         
-        
+        </li>
+    </ul>
+
+    <ul class="content-ul-data-serv">
+        <li>
+            
+           <div class="color-text-grey-light"style="padding-top: 10px" >3 – Após sua aprovação, iremos às compras para você! Compraremos tudo que você pediu e cadastramos tudo em sua conta KeepBox para você conferir e acompanhar suas compras.
+</div> 
+        </li>
+    </ul>
+
+    <ul class="content-ul-data-serv">
+        <li>
+            
+           <div class="color-text-grey-light"style="padding-top: 10px" >4 – Então, unificaremos tudo que você desejar em uma só caixa, você seleciona os serviços extras que deseja e nós enviamos tudo para seu endereço no Brasil. 
+</div> 
+        </li>
+    </ul>
+
+
+</div>
+
 
 
 
