f7029364f78ae98880edec2b07212c0ba3e97ffc
mdd300 <victor.za.oshiro5@gmail.com>
2018-05-08 17:01:26 -0300

    ajustes

--- application/views/Keep/structure/header_default.php
+++ application/views/Keep/structure/header_default.php
@@ -98,12 +98,17 @@ License: You must have a valid license purchased only from themeforest(the above
 
     <div class="modal-login" ng-show="login_press">
         <div class="text-font-sans text-title-sm color-text-green text-bold " style="float: left; width: 66%;margin: 20px;">Meu Acesso</div>
-        <div class="color-text-roxo text-bold, text-font-sans" style="border-radius: 200px;
+        <div class="color-text-roxo text-bold, text-font-sans" style="
+    border-radius: 200px;
     float: right;
     cursor: pointer;
     margin: 35px;
     border: 1px solid;
-    padding: 7px 11px;" ng-click="login_press = false">X</div>
+    padding: 7px 11px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+" ng-click="login_press = false">X</div>
         <div class="content-login">
             <div class="text-bold text-font-sans color-text-grey-light text-1">Usuário</div>
             <input class="input" type="text" ng-model="singIn.user_login" style="margin-top: 10px !important;" placeholder="Digite seu usuário ou E-mail">
@@ -114,13 +119,12 @@ License: You must have a valid license purchased only from themeforest(the above
             <div ng-show="Error_login">{{TextError_Login}}</div>
         </div>
         <div style="    margin-left: 20px;">
-        <button class="btn-config color-background-green color-text-white text-bold text-font-sans text-1-sm" ng-click="login()"><div class="loader-roxo" ng-show="loader_login"></div> Entrar </button>
-        <a href="<?= base_url('home/cadastroPage' )?>">
-            <button class="btn-config color-background-roxo color-text-white text-bold text-font-sans text-1-sm"> Novo acesso </button>
-        </a>
+        <button class="btn-config-2 color-background-green color-text-white text-bold text-font-sans text-1-sm" ng-click="login()"><div class="loader-roxo" ng-show="loader_login"></div> Entrar </button>
         </div>
+        <div style="    margin-left: 20px; margin-top: 10px"><a href="<?= base_url('home/cadastroPage' )?>">
+            <button class="btn-config-2 color-background-roxo color-text-white text-bold text-font-sans text-1-sm"> Novo acesso </button>
+            </a></div>
     </div>
-
 
 </div>
 
