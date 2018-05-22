6ae0cba26e2dd15adc21fb31cf581fc0ba7d09dc
mdd300 <victor.za.oshiro5@gmail.com>
2018-05-22 00:15:00 -0300

    ajustes

--- application/views/Keep/structure/header_default.php
+++ application/views/Keep/structure/header_default.php
@@ -16,7 +16,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- begin::Head -->
 <head>
     <meta charset="utf-8"/>
-    <meta name="viewport" content="width=1960">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="google-site-verification" content="gJupZ3IAxhkzShW-a0VDNkb4MiX9oPps_DgcOYJg_QI" />
     <title>
         KeepBox
